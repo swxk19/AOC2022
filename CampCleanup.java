ba923ac72e9d5b95a3ddd72afda3c7a909190a78
swxk19 <s.wxk19@gmail.com>
2022-12-07 20:15:07 +0800

    Solve day 4

---
+++ CampCleanup.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class CampCleanup {
+    public static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    public static PrintWriter output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    public static String line = "init";
+
+    public static void main(String args[]) throws IOException {
+        int count = 0;
+
+        while (!line.equals("end")) {
+            if (!line.equals("init")) {
+                String[] temp1 = line.split(",");
+                String[] temp2 = temp1[0].split("-");
+                String[] temp3 = temp1[1].split("-");
+
+                int[] firstElf = {Integer.parseInt(temp2[0]), Integer.parseInt(temp2[1])};
+                int[] secondElf = {Integer.parseInt(temp3[0]), Integer.parseInt(temp3[1])};
+
+                if (firstElf[0] <= secondElf[0] && secondElf[1] <= firstElf[1]) {
+                    count += 1;
+                } else if (secondElf[0] <= firstElf[0] && firstElf[1] <= secondElf[1]) {
+                    count += 1;
+                }
+            }
+            line = input.readLine();
+        }
+        output.println(count);
+        output.close();
+        input.close();
+    }
+
+
+}
